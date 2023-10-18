e70041f90939f02bb6ce17650e4c48f1cda154d9
danielasl2 <2157510@brunel.ac.uk>
2023-10-18 09:44:04 +0100

    showing cookies

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,23 @@
 
-function getAllCookies() {
-    chrome.cookies.getAll({ url: 'https://https://www.youtube.com/"' }, function(cookies) {
-      console.log(cookies);
+document.getElementById('showCookies').addEventListener('click', function() {
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var currentTab = tabs[0];
+    chrome.cookies.getAll({ url: currentTab.url }, function(cookies) {
+      displayCookies(cookies);
+    });
+  });
+});
+
+function displayCookies(cookies) {
+  var cookieList = document.getElementById('listAllCookies');
+  cookieList.innerHTML = '<h2>Cookies:</h2>';
+  if (cookies.length === 0) {
+    cookieList.innerHTML += '<p>No cookies found.</p>';
+  } else {
+    cookies.forEach(function(cookie) {
+      cookieList.innerHTML += '<p><strong>' + cookie.name + ':</strong> ' + cookie.value + '</p>';
     });
   }
-  
-  getAllCookies();
-  
+
+}
