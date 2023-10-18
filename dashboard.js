133538ed367bf090f39dd20c71573a9fe478ed44
danielasl2 <2157510@brunel.ac.uk>
2023-10-18 10:18:29 +0100

    improving code

--- dashboard.js
+++ dashboard.js
@@ -9,15 +9,19 @@ document.getElementById('showCookies').addEventListener('click', function() {
   });
 });
 
-function displayCookies(cookies) {
-  var cookieList = document.getElementById('listAllCookies');
-  cookieList.innerHTML = '<h2>Cookies:</h2>';
-  if (cookies.length === 0) {
-    cookieList.innerHTML += '<p>No cookies found.</p>';
-  } else {
-    cookies.forEach(function(cookie) {
-      cookieList.innerHTML += '<p><strong>' + cookie.name + ':</strong> ' + cookie.value + '</p>';
-    });
-  }
+ function displayCookies(cookies) {
+   var cookieList = document.getElementById('listAllCookies');
+   var htmlString = '<h2>Cookies:</h2>';
+
+   if (cookies.length === 0) {
+     htmlString += '<p>No cookies found.</p>';
+   } else {
+     htmlString += cookies.map(function(cookie) {
+       return `<p><strong>${cookie.name}:</strong> ${cookie.value}</p>`;
+     }).join('');
+   }
+
+   cookieList.innerHTML = htmlString;
+ }
 
 }
