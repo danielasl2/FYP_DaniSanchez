fb39ad3da1574ea3dce46a10b48b389a1b8648e7
danielasl2 <2157510@brunel.ac.uk>
2023-12-18 13:29:54 +0000

    fix expiration date and merge some categorise together

--- cookies-frontend/src/reuse/utils.js
+++ cookies-frontend/src/reuse/utils.js
@@ -1,6 +1,8 @@
 export function formatExpirationDate(timestamp) {
-    if (!timestamp) return 'N/A';
-    const date = new Date(timestamp * 1000);
-    if (isNaN(date.getTime())) return 'Invalid Date';
-    return date.toLocaleDateString();
-  }
+  console.log('Timestamp received:', timestamp);
+  if (!timestamp) return 'N/A';
+  const date = new Date(timestamp * 1000);
+  console.log('Converted Date:', date);
+  if (isNaN(date.getTime())) return 'Invalid Date';
+  return date.toLocaleDateString();
+}
