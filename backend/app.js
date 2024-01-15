80ede768870a013fcf55cf8ecbb71030e42423d3
danielasl2 <2157510@brunel.ac.uk>
2024-01-15 10:59:52 +0000

    mongodb connected to node.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mongoose = require('mongoose');
+const mongoDBUri = 'mongodb://localhost:27017/cookiesDB' ; 
+
+mongoose.connect(mongoDBUri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,4 +13,16 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+db.on('disconnected', function() {
+    console.log('MongoDB disconnected');
+});
+db.on('reconnected', function () {
+    console.log('MongoDB reconnected');
 });
