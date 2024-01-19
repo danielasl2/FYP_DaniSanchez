a919693da240e513eb098542ead49569e460bfaa
danielasl2 <2157510@brunel.ac.uk>
2024-01-19 09:59:00 +0000

    changes

--- cookies-frontend/src/api.js
+++ cookies-frontend/src/api.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://localhost:3000';
 
 const api = {
     getCookies() {
-        return axios.get(`${API_URL}/cookies`);
+        return axios.get(`${API_URL}`);
     },
     updateCookieStatus(cookieId, blockedStatus) {
         return axios.patch(`${API_URL}/cookies/block/${cookieId}`, { blockedStatus });
