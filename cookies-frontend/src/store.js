b8a760665226a0b2750901f18953ea101403ec81
danielasl2 <2157510@brunel.ac.uk>
2024-02-19 15:36:35 +0000

    cookies not fixed yet

--- cookies-frontend/src/store.js
+++ cookies-frontend/src/store.js
@@ -1,19 +1,21 @@
 import { createStore } from 'vuex';
 import api from './api';
+import { cookieMixin} from './mixin/cookieMix'
 
 const store = createStore({
     state: {
-        cookies: []
+        cookies: {}
     },
     mutations: {
         UPDATE_COOKIE_STATUS(state, updatedCookie) {
-            const index = state.cookies.findIndex(cookie => cookie._id === updatedCookie._id);
+            const category = cookieMixin(updatedCookie);
+            const index = state.cookies[category]?.findIndex(cookie => cookie._id === updatedCookie._id);
             if (index !== -1) {
-                state.cookies.splice(index, 1, updatedCookie);
+                state.cookies[category][index] = updatedCookie;
             }
         },
-        SET_COOKIES(state, cookies){
-            state.cookies = cookies
+        SET_COOKIES(state, cookieMix){
+            state.cookies = cookieMix;
         },
     },
     actions: {
@@ -33,9 +35,18 @@ const store = createStore({
                    // console.log("Retrieved userId:", userId);
                 }
     
-                const response = await api.getCookies(userId); 
-                console.log("Fetched cookies:", response);
-                commit('SET_COOKIES', response);
+                const rawCookies = await api.getCookies(userId); 
+                const cookieMix= {};
+
+                rawCookies.forEach(cookie => {
+                    const category = cookieMixin(cookie);
+                    if(! cookieMix[category]){
+                        cookieMix[category] = [];
+                    }
+                    cookieMix[category].push(cookie);
+                })
+               // console.log("Fetched cookies:", response);
+                commit('SET_COOKIES', cookieMix);
             } catch (error) {
                 console.error('Error fetching cookies:', error);
             }
